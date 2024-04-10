2b17f088b29d3c09ba7245cc8b967c5e0812441f
ThibaultGdf <107555179+ThibaultGdf@users.noreply.github.com>
2024-04-10 17:57:25 +0200

    [FEAT] Database

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(`postgres://${process.env.DEV_DB_USER}:${process.env.DEV_DB_PASSWORD}@${process.env.DEV_DB_HOST}:${process.env.PORT_DB}/${process.env.DEV_DB_NAME}`);
+
+try {
+  sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+  });
+  
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
